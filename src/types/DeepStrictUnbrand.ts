2460e44b06fbb58c2f1b343e4ea53c42a0db433f
kakasoo <kscodebase@gmail.com>
2024-08-25 22:40:41 +0900

    fix: fix Unbrand type

--- src/types/DeepStrictUnbrand.ts
+++ src/types/DeepStrictUnbrand.ts
@@ -1,7 +1,17 @@
 type Primitive = string | number | boolean | symbol | null | undefined;
-type Unbrand<T extends Primitive & NonNullable<unknown>> = T extends Primitive &
-    NonNullable<unknown>
-    ? Extract<Primitive, T>
+type Unbrand<T extends Primitive & Record<any, any>> = T extends string &
+    Record<any, any>
+    ? Extract<string, Omit<T, any>>
+    : T extends number & Record<any, any>
+    ? Extract<number, Omit<T, any>>
+    : T extends boolean & Record<any, any>
+    ? Extract<boolean, Omit<T, any>>
+    : T extends symbol & Record<any, any>
+    ? Extract<symbol, Omit<T, any>>
+    : T extends null & Record<any, any>
+    ? Extract<null, Omit<T, any>>
+    : T extends undefined & Record<any, any>
+    ? Extract<undefined, Omit<T, any>>
     : never;
 
 export type DeepStrictUnbrand<T> = T extends Array<Date>
@@ -13,9 +23,5 @@ export type DeepStrictUnbrand<T> = T extends Array<Date>
     : T extends Date
     ? T
     : {
-          [K in keyof T]: T[K] extends Unbrand<infer I>
-              ? Extract<I, T[K]> // Primitive type에서 T[K]의 부분 집합만을 추려내서 브랜드 타입을 제거
-              : T[K] extends object
-              ? DeepStrictUnbrand<T[K]>
-              : T[K];
+          [K in keyof T]: T[K] extends object ? DeepStrictUnbrand<T[K]> : T[K];
       };
